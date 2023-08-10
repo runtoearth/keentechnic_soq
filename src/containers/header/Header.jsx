ed9749c5353ce934d35bf6caa438804ca17b12a7
runtoearth <safwaanr2e@gmail.com>
2023-08-10 14:24:50 -0400

    commit first

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -11,13 +11,7 @@ const Header = () => {
         <p>As a client-centric consultancy, KeenTechnic goes beyond creating visually stunning websites. Their team understands the crucial role of user experience and user interface in driving success on the digital landscape. By combining artistic flair with cutting-edge technology, KeenTechnic crafts websites that are not only visually striking but also seamlessly responsive across all devices.</p>
 
         <div className="gpt3__header-content__input">
-          <input type="email" placeholder="Your Email Address" />
-          <button type="button">Get Started</button>
-        </div>
-
-        <div className="gpt3__header-content__people">
-          <img src={people} alt="people" />
-          <p>1,600 people requested access a visit in last 24 hours</p>
+          <button type="button">Start A Project</button>
         </div>
       </div>
       <div className="gpt3__header-image">
