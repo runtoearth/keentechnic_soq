10b60cc248255de0ad4ed08492c9b16ec39d2cd0
runtoearth <safwaanr2e@gmail.com>
2023-08-13 19:11:28 -0400

    commit first

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo.png';
 import './navbar.css';
 
 const Navbar = () => {
@@ -14,10 +14,10 @@ const Navbar = () => {
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <p><a href="#wgpt3">Our Solution</a></p>
+          <p><a href="#possibility">Recent Projects</a></p>
+          <p><a href="#features">Our Services</a></p>
+          <p><a href="#blog">Contact Us</a></p>
         </div>
       </div>
       <div className="gpt3__navbar-menu">
@@ -28,7 +28,7 @@ const Navbar = () => {
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#wgpt3">Our Solution</a></p>
             <p><a href="#possibility">Open AI</a></p>
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Library</a></p>
