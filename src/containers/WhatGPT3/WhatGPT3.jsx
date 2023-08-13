186517d5063841c5e3ab7dfa26c27dc6bc7a8a02
runtoearth <safwaanr2e@gmail.com>
2023-08-13 19:56:07 -0400

    commit first

--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -6,16 +6,16 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+        <Feature title="Recent Projects" text="A responsive and beautiful website design is crucial for KeenTechnic as it ensures a positive user experience. With a design that adapts seamlessly to different devices and screen sizes, visitors can access the website from desktops, tablets, and smartphones without any loss of functionality or aesthetic appeal." />
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <h1 className="gradient__text">Modern & Resposive Design</h1>
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. " />
-        <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-        <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+        <Feature title="Workforce Connect" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. " />
+        <Feature title="Algebra Teahouse" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+        <Feature title="Angels project forgot" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
       </div>
     </div>
   )
