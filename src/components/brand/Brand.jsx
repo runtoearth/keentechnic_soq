db3ed77381ee4ae9ee07e7670d63f27ce1d4a622
runtoearth <safwaanr2e@gmail.com>
2023-08-13 19:37:59 -0400

    commit first

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './brand.css';
-import { google, slack, atlassian, dropbox, shopify } from './import';
+import { slack, atlassian, dropbox, shopify } from './import';
 
 const Brand = () => {
   return (
     <div className='gpt3__brand section__padding'>
-      <div>
-        <img src={google} alt="google" />
-      </div>
       <div>
         <img src={slack} alt="slack" />
       </div>
