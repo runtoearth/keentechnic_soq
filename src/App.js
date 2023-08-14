2eb167ec7bbd1c156865aa2f96fef4f1979bb709
runtoearth <safwaanr2e@gmail.com>
2023-08-14 11:37:00 -0400

    Removed CTA section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
-import { CTA, Brand, Navbar } from './components';
+import { Brand, Navbar } from './components';
 import './App.css';
 
 const App = () => {
@@ -15,7 +15,6 @@ const App = () => {
             <WhatGPT3 />
             <Features />
             <Possibility />
-            <CTA />
             <Blog />
             <Footer />
         </div>
