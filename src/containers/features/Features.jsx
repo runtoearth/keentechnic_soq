a70f5b314a59699f7ec30085488fc3c4916eec29
webdev-angel <112652800+webdev-angel@users.noreply.github.com>
2023-08-18 12:20:09 -0400

    Form

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -6,15 +6,15 @@ import finart from '../../assets/finart.png';
 const featureData = [
   {
     title: 'Web Design',
-    text: 'KeenTechnics Tailored Web Design Expertise Discover the power of a well-executed web design project with KeenTechnics. Our phased approach guarantees a comprehensive consideration of all vital aspects, delivering a high-performance website. Our highly skilled team, each a specialist in their field, ensures a seamless and captivating online experience for your audience'
+    text: "KeenTechnic's Tailored Web Design Expertise Discover the power of a well-executed web design project with KeenTechnics. Our phased approach guarantees a comprehensive consideration of all vital aspects, delivering a high-performance website. Our highly skilled team, each a specialist in their field, ensures a seamless and captivating online experience for your audience"
   },
   {
     title: 'SEO Optimization',
-    text: 'Keentechnics All-Inclusive Digital Marketing Keentechnic goes beyond the norm, providing a rare in-house advantage in digital marketing and SEO services. By combining website optimizations with SEO strategies, we create a seamless and productive collaboration. Embrace efficiency and unlock your websites full potential with Keentechnic as your dedicated digital partner. '
+    text: "Keentechnic's All-Inclusive Digital Marketing Keentechnic goes beyond the norm, providing a rare in-house advantage in digital marketing and SEO services. By combining website optimizations with SEO strategies, we create a seamless and productive collaboration. Embrace efficiency and unlock your websites full potential with Keentechnic as your dedicated digital partner."
   },
   {
     title: 'Website Hosting',
-    text: 'Keentechnic offers a powerful and reliable website hosting solution that combines cutting-edge technology, top-tier security, and unparalleled support. With our hosting services, your web design business can thrive in the digital landscape, delivering a seamless and engaging user experience to your audience while enjoying the peace of mind that comes with a dependable hosting partner. Experience the Keentechnic advantage and take your web design business to new heights with our exceptional website hosting services. '
+    text: "Keentechnic offers a powerful and reliable website hosting solution that combines cutting-edge technology, top-tier security, and unparalleled support. With our hosting services, your web design business can thrive in the digital landscape, delivering a seamless and engaging user experience to your audience while enjoying the peace of mind that comes with a dependable hosting partner. Experience the Keentechnic advantage and take your web design business to new heights with our exceptional website hosting services."
   },
 ]
 
